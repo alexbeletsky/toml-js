ac1aa05dbad08bd30c39df9a866670ad76840571
alexander.beletsky <alexander.beletsky@gmail.com>
2013-02-24 20:56:35 +0200

    toml object added

---
+++ src/toml.js
@@ -0,0 +1,7 @@
+var toml = (function () {
+
+    return {
+
+    };
+
+})();
---
+++ test/spec/toml.spec.js
@@ -0,0 +1,9 @@
+describe('toml.js spec', function () {
+
+    describe('toml construction', function () {
+        it ('should exist', function () {
+            expect(toml).to.be.ok;
+        })
+    });
+
+});
