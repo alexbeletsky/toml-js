ddcab0a9787c446b9e1f30ff14c5b655ca5f5623
Vitaly Domnikov <dotcypress@gmail.com>
2013-08-05 15:27:36 +0300

    dump support

--- src/toml.js
+++ src/toml.js
@@ -204,8 +204,80 @@ var toml = (function () {
         return context.result;
     };
 
+    String.prototype.replaceAll = function (find, replace) {
+        var str = this;
+        return str.replace(new RegExp(find, 'g'), replace);
+    };
+
+    var escapeString = function(str) {
+        return str
+            .replaceAll('\b', '\\b')
+            .replaceAll('\t', '\\t')
+            .replaceAll('\n', '\\n')
+            .replaceAll('\f', '\\f')
+            .replaceAll('\r', '\\r')
+            .replaceAll('\"', '\\"');
+    };
+
+    var isSimpleType = function(value){
+        var type = typeof value;
+        var strType = Object.prototype.toString.call(value);
+        return type == 'string' || type == 'number' || type == 'boolean' || strType == '[object Date]' || strType == '[object Array]';
+    };
+
+    var dumpObject = function(value, context) {
+        context = context || [];
+        var type = Object.prototype.toString.call(value);
+        if(type === '[object Date]') {
+            return value.toISOString();
+        } else if(type === '[object Array]' ) {
+            if(value.length == 0){
+                return null;
+            }
+            var result = '[';
+            for (var index = 0; index < value.length; ++index) {
+               result += dump(value[index]) + ', ';
+            }
+            return result.substring(0, result.length - 2) + ']';
+        }
+        var result = '';
+        var simleProps = '';
+        for(var propertyName in value) {
+            if(isSimpleType(value[propertyName])){
+                simleProps += propertyName + ' = ' + dump(value[propertyName]) + '\n';
+            }
+        }
+        if(simleProps){
+            if(context.length > 0){
+               var contextName = context.join('.');
+               result += '[' + contextName + ']\n';
+            }
+            result += simleProps + '\n';
+        }
+        for(var propertyName in value) {
+            if(!isSimpleType(value[propertyName])){
+                result += dump(value[propertyName], context.concat(propertyName));
+            }
+        }
+        return result;
+    };
+
+    var dump = function(value, context) {
+        switch (typeof value) {
+            case 'string':
+                return '"' + escapeString(value) + '"';
+            case 'number':
+                return '' + value;
+            case 'boolean':
+                return value ? 'true' : 'false';
+            case 'object':
+                return dumpObject(value, context);
+        }
+    };
+
     return {
-        parse: startParser
+        parse: startParser,
+        dump: dump
     };
 
 })();
